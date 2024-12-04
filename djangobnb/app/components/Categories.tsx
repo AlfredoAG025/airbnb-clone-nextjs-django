616aefa288943d4bc9a26201367acf466fec39b6
José Alfredo Arroyo Gómez <arroyo.alfredo.1pm@gmail.com>
2024-12-03 20:53:49 -0600

    feat: Add Categories component (static)

---
+++ djangobnb/app/components/Categories.tsx
@@ -0,0 +1,40 @@
+export const Categories = () => {
+    return (
+        <div className="pt-3 cursor-pointer pb-6 flex items-center gap-x-12">
+            <div className="pb-4 flex flex-col items-center gap-y-2 border-b-2 border-white hover:border-gray-200 opacity-60 hover:opacity-100">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" width={24} height={24} strokeWidth={2}> <path d="M17.553 16.75a7.5 7.5 0 0 0 -10.606 0"></path> <path d="M18 3.804a6 6 0 0 0 -8.196 2.196l10.392 6a6 6 0 0 0 -2.196 -8.196z"></path> <path d="M16.732 10c1.658 -2.87 2.225 -5.644 1.268 -6.196c-.957 -.552 -3.075 1.326 -4.732 4.196"></path> <path d="M15 9l-3 5.196"></path> <path d="M3 19.25a2.4 2.4 0 0 1 1 -.25a2.4 2.4 0 0 1 2 1a2.4 2.4 0 0 0 2 1a2.4 2.4 0 0 0 2 -1a2.4 2.4 0 0 1 2 -1a2.4 2.4 0 0 1 2 1a2.4 2.4 0 0 0 2 1a2.4 2.4 0 0 0 2 -1a2.4 2.4 0 0 1 2 -1a2.4 2.4 0 0 1 1 .25"></path> </svg>
+
+
+                <span className="text-xs">Beach</span>
+            </div>
+
+            <div className="pb-4 flex flex-col items-center gap-y-2 border-b-2 border-white hover:border-gray-200 opacity-60 hover:opacity-100">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" width={20} height={20} strokeWidth={2}> <path d="M16.95 9.01c3.02 .739 5.05 2.123 5.05 3.714c0 2.367 -4.48 4.276 -10 4.276s-10 -1.909 -10 -4.276c0 -1.59 2.04 -2.985 5.07 -3.724"></path> <path d="M7 9c0 1.105 2.239 2 5 2s5 -.895 5 -2v-.035c0 -2.742 -2.239 -4.965 -5 -4.965s-5 2.223 -5 4.965v.035"></path> <path d="M15 17l2 3"></path> <path d="M8.5 17l-1.5 3"></path> <path d="M12 14h.01"></path> <path d="M7 13h.01"></path> <path d="M17 13h.01"></path> </svg>
+
+
+                <span className="text-xs">U.F.O</span>
+            </div>
+
+            <div className="pb-4 flex flex-col items-center gap-y-2 border-b-2 border-white hover:border-gray-200 opacity-60 hover:opacity-100">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" width={24} height={24} strokeWidth={2}> <path d="M5 12l-2 0l9 -9l9 9l-2 0"></path> <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7"></path> <path d="M10 12h4v4h-4z"></path> </svg>
+
+
+                <span className="text-xs">Tiny Homes</span>
+            </div>
+
+            <div className="pb-4 flex flex-col items-center gap-y-2 border-b-2 border-white hover:border-gray-200 opacity-60 hover:opacity-100">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" width={24} height={24} strokeWidth={2}> <path d="M5 12l-2 0l9 -9l9 9l-2 0"></path> <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7"></path> <path d="M10 12h4v4h-4z"></path> </svg>
+
+
+                <span className="text-xs">Tiny Homes</span>
+            </div>
+
+            <div className="pb-4 flex flex-col items-center gap-y-2 border-b-2 border-white hover:border-gray-200 opacity-60 hover:opacity-100">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" width={24} height={24} strokeWidth={2}> <path d="M5 3h1a1 1 0 0 1 1 1v2h3v-2a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v2h3v-2a1 1 0 0 1 1 -1h1a1 1 0 0 1 1 1v4.394a2 2 0 0 1 -.336 1.11l-1.328 1.992a2 2 0 0 0 -.336 1.11v7.394a1 1 0 0 1 -1 1h-10a1 1 0 0 1 -1 -1v-7.394a2 2 0 0 0 -.336 -1.11l-1.328 -1.992a2 2 0 0 1 -.336 -1.11v-4.394a1 1 0 0 1 1 -1z"></path> <path d="M10 21v-5a2 2 0 1 1 4 0v5"></path> </svg>
+
+
+                <span className="text-xs">Castles</span>
+            </div>
+        </div>
+    );
+}
